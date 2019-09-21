957e51a7d81b8687672075b94500f426298d64c5
Michael Gatuma <50529359+MichaelGatuma@users.noreply.github.com>
2019-09-21 07:55:13 +0300

    Create necessary migrations

---
+++ database/migrations/2019_09_20_111230_create_loans_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLoansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('borrowerid');
+            $table->foreign('borrower_id')
+                  ->references('id')->on('borrowers');
+            $table->text('term');
+            $table->longtext('details');
+            $table->unsignedInteger('amount');
+            $table->unsignedInteger('interest_rate');
+            $table->dateTime('applied_at');
+            $table->dateTime('disbursed_at');
+            $table->date('repayment_deadline');
+            $table->text('loan_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+}
