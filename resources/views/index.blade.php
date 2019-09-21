8b4d2f75d958eb42f9a1ee2060504b24e77c5a67
Michael Gatuma <50529359+MichaelGatuma@users.noreply.github.com>
2019-09-21 15:44:30 +0300

    Add Homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    
+    <div class="jumbotron text-center">
+        <h1>Welcome {{$name}}</h1><br/>
+        <p>Superview Microfinance Online System</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a>
+        <a class="btn btn-success btn-lg" href="#" role="button">Register</a>
+        </p>
+    </div>
+@endsection
