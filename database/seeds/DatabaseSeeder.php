e42f2b03e277a2d22c8391767df36f1e3408c35a
Michael Gatuma <50529359+MichaelGatuma@users.noreply.github.com>
2019-09-30 18:10:48 +0300

    Revert "Install NowUi them"
    This reverts commit 4aae4071823bd804a53c1bc687f9fe984a617e5c.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,18 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0');
-
-        DB::table('users')->truncate();
-
-        $this->call([ UsersTableSeeder::class]);
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+        // $this->call(UsersTableSeeder::class);
     }
 }
