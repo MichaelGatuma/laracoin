7008150bb589936f11339492fd191fe2cf3ede8b
Michael Gatuma <50529359+MichaelGatuma@users.noreply.github.com>
2019-10-02 01:24:02 +0300

    Customize placeholding texts

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,24 +2,24 @@
   <div class=" container-fluid ">
     <nav>
       <ul>
-        <li>
-          <a href="https://www.creative-tim.com" target="_blank">
-            {{__(" Creative Tim")}}
+        <li>Powered by
+          <a href="#" target="_blank">
+            {{__(" Techphile Developers")}}
           </a>
         </li>
         <li>
-          <a href="http://presentation.creative-tim.com" target="_blank">
-            {{__(" About Us")}}
+          <a href="#" target="_blank">
+            {{__(" About Superview")}}
           </a>
         </li>
         <li>
-          <a href="http://blog.creative-tim.com" target="_blank">
-            {{__(" Blog")}}
+          <a href="#" target="_blank">
+            {{__(" Landing Page")}}
           </a>
         </li>
         <li>
           <a href="https://www.updivision.com" target="_blank">
-            {{__(" Updivision")}}</a>
+            {{__(" Developer Feedback")}}</a>
         </li>
       </ul>
     </nav>
@@ -28,9 +28,9 @@
       <script>
         document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
       </script>, {{__(" Designed by")}}
-      <a href="https://www.invisionapp.com" target="_blank">{{__(" Invision")}}</a>{{__(" . Coded by")}}
-      <a href="https://www.creative-tim.com" target="_blank">{{__(" Creative Tim ")}}</a>&
-      <a href="https://www.updivision.com" target="_blank">{{__(" Updivision")}}</a>
+      <a href="#" target="_blank">{{__(" Techphile")}}</a>{{__(" . Coded by")}}
+      <a href="#" target="_blank">{{__(" MiGA Systems ")}}</a>&
+      <a href="#" target="_blank">{{__(" Updivision")}}</a>
     </div>
   </div>
 </footer>
